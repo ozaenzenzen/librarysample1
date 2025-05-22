6d1c83b47342d89379f42e43aba0f848694d1d99
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-05-22 14:23:51 +0700

    update: add again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ afterEvaluate {
                 groupId = "com.github.ozaenzenzen"
                 artifactId = "librarysample1"
                 version = "1.0.0"
-//                pom(splitPOM)
+                pom(splitPOM)
             }
         }
 
